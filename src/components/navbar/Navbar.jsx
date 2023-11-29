837b5251dda0d07db63600fc461f953a72581d58
Luis Pablo <pabloarias024@gmail.com>
2023-11-29 13:12:39 -0300

    Actualización de logout

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,16 +1,32 @@
-import {Link} from 'react-router-dom'
+import {Link, useNavigate} from 'react-router-dom'
 import "./navbar.css"
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { AuthContext } from '../../context/AuthContext';
-
+import toast, { Toaster } from "react-hot-toast";
 
 const image =
   "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.25&w=256&h=256&q=80";
 
 const Navbar = () => {
+  const [openProfile, setOpenProfile] = useState(false)
+  const { user, dispatch } = useContext(AuthContext);
 
-  const { user } = useContext(AuthContext);
+  const navigate = useNavigate()
 
+  const logout = () => {
+    try {
+      dispatch({ type: "LOGOUT" });
+      toast.success("Cerrando sesión", {
+        duration: 1500,
+        position: "top-center",
+      });
+      setTimeout(() => {
+        navigate("/");
+      }, 1500);
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
   return (
     <div className="navbar">
@@ -19,12 +35,27 @@ const Navbar = () => {
           <span className="logo">Reservas online</span>
         </Link>
         {user ? (
-          <div className="navUserIsLog">
-            <div className="navImg">
-              <img src={image} alt="" />
+          <>
+            <div className="navUserIsLog">
+              <div className="navImg">
+                <img
+                  src={image}
+                  alt=""
+                  onClick={() => setOpenProfile((prev) => !prev)}
+                />
+              </div>
+              <div className="navUser">
+                <span>{user.username}</span>
+                {openProfile && (
+                  <ul className="navDropDown">
+                    <li>
+                      <span onClick={logout}>Cerrar sesión</span>
+                    </li>
+                  </ul>
+                )}
+              </div>
             </div>
-            <div className="navUser">{user.username}</div>
-          </div>
+          </>
         ) : (
           <div className="navItems">
             <Link to="/register" className="navButton">
@@ -36,6 +67,7 @@ const Navbar = () => {
           </div>
         )}
       </div>
+      <Toaster/>
     </div>
   );
 }
